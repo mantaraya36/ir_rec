e1b1c01ba48ec68f60efa6b1a9fc7bb9b567883a
allolab <create@bossanova.local>
2013-05-16 11:27:52 -0700

    make sweep script can now run without numpy/scipy

--- scripts/make_sweep.py
+++ scripts/make_sweep.py
@@ -2,9 +2,15 @@ sr = 48000
 dur = 3
 fadein = 0.1
 fadeout = 0.03
-fmin = 20
+fmin = 50
 fmax = 20000
 out_name = 'sweep.wav'
+
+try:
+    from pylab import *
+except:
+    from math import *
+
 
 # from Aliki - GPL
 totalsamps = float(sr * dur)
@@ -16,7 +22,7 @@ a = fmin / (b * sr);
 r = 0.5 * a * (fmax / fmin) * (_k1 + 0.5 * (_k0 + _k2)) / (b * _k1);
 q0 = a * exp (-b * _k0);
 sweep = []
-for i in arange(-_k0, _k1 + _k2):
+for i in range(-_k0, _k1 + _k2):
     if i < 0:   g = cos (0.5 * pi * i / _k0);
     elif i < _k1: g = 1.0;
     else:  g = sin (0.5 * pi * (_k1 + _k2 - i) / _k2);
@@ -24,6 +30,18 @@ for i in arange(-_k0, _k1 + _k2):
     p = q - q0;
     x = g * sin (2 * pi * (p - floor (p)));
     sweep.append(x)
-    
-from scipy.io import wavfile
-wavfile.write(out_name, sr, (array(sweep)*2**15).astype(int16))
+
+try:
+    from scipy.io import wavfile
+    wavfile.write(out_name, sr, (array(sweep)*2**15).astype(int16))
+except:
+    import wave
+    import struct
+    wav_file = wave.open(out_name, "w")
+    comptype = "NONE"
+    compname = "not compressed"
+    wav_file.setparams((1, 2, sr, len(sweep), comptype, compname))
+    for s in sweep:
+    # write the audio frames to file
+        wav_file.writeframes(struct.pack('h', int(s*(2**15))))
+    wav_file.close()
